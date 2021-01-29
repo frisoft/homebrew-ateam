f9fef90a406041c7f99ed676d1bdb04b78cd44aa
Alkesh Vaghmaria <alkesh.vaghmaria@smartpension.co.uk>
2021-01-29 09:11:15 +0000

    update download location to frisoft

--- Formula/ateam.rb
+++ Formula/ateam.rb
@@ -1,8 +1,8 @@
 class Ateam < Formula
   desc "A tool that helps optimize the code review process"
   homepage "https://github.com/frisoft/ateam"
-  url "https://github.com/alkesh/ateam/releases/download/v0.7.0/ateam.tar.gz"
-  sha256 "0d91eae42c6abfd2ef0a99c8b25c9730c043c1bc2658e469720d0a25abb5c718"
+  url "https://github.com/frisoft/ateam/releases/download/0.7.0/ateam-0.7.0-x86_64-apple-darwin.tar.gz"
+  sha256 "4f7b94aadb6ca961f31a7a970fdb604695d572ecd0c0fe0335fb50e857aa89ed"
   version "0.7.0"
 
   def install
