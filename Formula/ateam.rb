5b8f750479e75e11fdbcb21394b62bd51e415001
Andrea Frigido <frisoft@users.noreply.github.com>
2021-09-07 23:57:02 +0100

    v0.8.2

--- Formula/ateam.rb
+++ Formula/ateam.rb
@@ -1,9 +1,9 @@
 class Ateam < Formula
   desc "A tool that helps optimize the code review process"
   homepage "https://github.com/frisoft/ateam"
-  url "https://github.com/frisoft/ateam/releases/download/0.8.0/ateam-0.8.0-x86_64-apple-darwin.tar.gz"
-  sha256 "928874975524f1da350ad77598cde51164f5f1eed8647982809642b721e8eaaf"
-  version "0.8.0"
+  url "https://github.com/frisoft/ateam/releases/download/0.8.2/ateam-0.8.2-x86_64-apple-darwin.tar.gz"
+  sha256 "0001dc0f7bb385865014f3b04b83b35ee6de8b11a8f2ac2866e04a58bc0f8280"
+  version "0.8.2"
 
   def install
     bin.install "ateam"
